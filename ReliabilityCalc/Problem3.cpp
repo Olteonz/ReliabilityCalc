afd88b8127c9353285815eab4930c5fdfdbac16a
Olteonz <olteonz@gmail.com>
2020-11-24 17:10:29 +0100

    Problem 3 changed to solve Bayes Formula

--- ReliabilityCalc/Problem3.cpp
+++ ReliabilityCalc/Problem3.cpp
@@ -4,12 +4,13 @@
 #include <iostream>
 #include <vector>
 #include "ReliabilitySystem.h"
+#include <cmath>
 
-/*Problem 3 from ch17pp.doc - Find the reliability of this system:*/
+/*Example 1.1-1.3 Bayes Formula from QITT03 - Reliability Test Planning & Weibull Analysis.pdf */
 //Aleksander Mielczarek
 using namespace std;
 
-float reliabilityOfSystem(RealiabilityNode starting) {
+/*float reliabilityOfSystem(RealiabilityNode starting) {
 	RealiabilityNode current = starting;
 	float R = 1;
 	while (true) {
@@ -53,7 +54,7 @@ void Problem3::solve() {
 	node3.reliability = 0.90;
 
 	node3b.reliability = 0.90;
-	std::cout << "\nReliability of system from problem 3 in ch17pp.doc is: " << reliabilityOfSystem(node1)*100<<"%";*/
+	std::cout << "\nReliability of system from problem 3 in ch17pp.doc is: " << reliabilityOfSystem(node1)*100<<"%";
 	vector<RealiabilityNode> nodes;
 	cout << "This calculator will output the reliability of a given system\nInput the number of nodes: ";
 	int numberOfNodes;
@@ -109,5 +110,78 @@ void Problem3::solve() {
 	cout << "\nReliability of system from problem 3 in ch17pp.doc is: " << reliabilityOfSystem(nodes[doesFirstNodeBranch]) * 100 << "%";
 	
 	
+};*/
+class BayesFormula {
+public:
+	float GetConfidence(float reliability, int sample_size) {
+		return 1 - pow(reliability, sample_size+1);
+	}
+	float GetReliability(float confidence, int sample_size) {
+		return pow(1 - confidence, 1.0 / (sample_size + 1));
+	}
+	float GetSampleSize(float confidence, float reliability) {
+		return log(1 - confidence) /log(reliability) - 1;
+	}
 };
 
+
+void Problem3::solve() {
+	BayesFormula formula;
+	cout << "This calculator will calculate either the reliability, confidence or sample size given the other values using the Bayes Formula\n input which value to find: 1 - Confidence, 2 - Reliability, 3 - Sample Size, other keys - exit: ";
+	int e;
+	cin >> e;
+	float confidence;
+	float reliability;
+	int sample_size;
+	switch (e)
+	{
+	case 1:
+		cout << "Please input the reliability: ";
+		while (!(cin >> reliability)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input.  Try again: ";
+		}
+		cout << "Please input the sample size: ";
+		while (!(cin >> sample_size)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input.  Try again: ";
+		}
+		cout << "The confidence is: " << formula.GetConfidence(reliability, sample_size) * 100 << "%\n";
+		break;
+	case 2:
+		cout << "Please input the confidence: ";
+		while (!(cin >> confidence)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input.  Try again: ";
+		}
+		cout << "Please input the sample size: ";
+		while (!(cin >> sample_size)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input.  Try again: ";
+		}
+		cout << "The reliability is: " << formula.GetReliability(confidence, sample_size) * 100 << "%\n";
+		break;
+	case 3:
+		cout << "Please input the confidence: ";
+		while (!(cin >> confidence)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input.  Try again: ";
+		}
+		cout << "Please input the reliability: ";
+		while (!(cin >> reliability)) {
+			cin.clear();
+			cin.ignore(numeric_limits<streamsize>::max(), '\n');
+			cout << "Invalid input.  Try again: ";
+		}
+		cout << "The sample size is: " << formula.GetSampleSize(confidence, reliability) << "\n";
+		break;
+	default:
+		break;
+	}
+
+}
